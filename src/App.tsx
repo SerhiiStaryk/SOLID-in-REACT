9bebd5f210579ed42a8f3af608e3057b6df27fb0
Serhii Staryk <serhii.staryk@gmail.com>
2024-05-13 00:04:57 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+type VideoDetails = {
+  previewUrl: string;
+  title: string;
+  author: string;
+};
+
+const loadVideoDetails = (id: string): Promise<VideoDetails> => {
+  return new Promise(resolve =>
+    setTimeout(
+      () =>
+        resolve({
+          previewUrl: 'https://i.ytimg.com/vi/3KxZeE9LXZo/sddefault.jpg',
+          title: 'Solid principles in React function components',
+          author: '@SerhiiStaryk',
+        }),
+      500
+    )
+  );
+};
+
+const VideoPreview = ({ videoId }: { videoId: string }) => {
+  const [videoDetails, setVideoDetails] = useState<VideoDetails>();
+
+  useEffect(() => {
+    loadVideoDetails(videoId).then(vd => setVideoDetails(vd));
+  }, [videoId]);
+
+  return videoDetails ? (
+    <div style={{ display: 'flex' }}>
+      <img
+        style={{ width: '200px', borderRadius: '10px', border: '1px solid' }}
+        src={videoDetails.previewUrl}
+        alt={videoDetails.title}
+      />
+      <div style={{ paddingLeft: '10px' }}>
+        <div style={{ fontWeight: 'bold' }}>{videoDetails.title}</div>
+        <div style={{ color: '#808080' }}>{videoDetails.author}</div>
+      </div>
+    </div>
+  ) : (
+    <span>...Loading</span>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <VideoPreview videoId={'testVideo'} />
     </div>
   );
 }
